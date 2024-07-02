3ebd0ceebe4dec2dd942065848ea6f56d1047ed6
elyas_h <elyas_h@kuber.local>
2024-07-02 20:26:31 +0330

    add header and footer files

--- src/App.vue
+++ src/App.vue
@@ -1,11 +1,20 @@
+<template>
+  <div class="">
+    <Header />
+    <router-view />
+    <Footer />
+  </div>
+</template>
+ 
 <script setup>
 import { RouterLink, RouterView } from 'vue-router'
+import Header from '@/components/Header.vue'
+import Footer from '@/components/Footer.vue'
+
 </script>
 
-<template>
-  <div>لورم ایپسوم </div>
+<style>
 
-  <RouterView />
-</template>
+</style>
 
 
---
+++ src/components/Header.vue
@@ -0,0 +1,49 @@
+<template>
+  <header dir="rtl" class="bg-light py-3">
+    <div
+      class="container-fluid d-flex justify-content-between align-items-center"
+    >
+      <img src="@/assets/img/achare-logo.png" alt="Logo" class="logo" />
+      <nav class="d-flex">
+        <router-link to="/" class="nav-link" :class="{ active: isActive('/address-list') }">
+          ثبت آدرس
+        </router-link>
+        <router-link
+          to="/address-list"
+          class="nav-link"
+          :class="{ active: isActive('/') }"
+        >
+          مشاهده آدرس‌ ها
+        </router-link>
+      </nav>
+    </div>
+  </header>
+  <!-- <div>Header</div> -->
+</template>
+
+<script setup>
+import { useRoute } from "vue-router";
+
+const route = useRoute();
+
+const isActive = (routePath) => {
+  return route.path === routePath;
+};
+</script>
+<style scoped>
+.logo {
+  height: 40px;
+}
+
+.nav-link {
+  text-decoration: none;
+  color: #333;
+  padding: 8px 16px;
+}
+
+.nav-link.active {
+  /* background-color: #007bff; */
+  color: #00BFA5;
+  /* border-radius: 4px; */
+}
+</style>
