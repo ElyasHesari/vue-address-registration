3a9c119e0991bb31532f8cac23fdfc45987ac3eb
elyas_h <elyas_h@kuber.local>
2024-07-02 20:25:11 +0330

    add bootstarp-icon to main.js

--- src/main.js
+++ src/main.js
@@ -4,8 +4,10 @@ import { createPinia } from 'pinia'
 import App from './App.vue'
 import router from './router'
 
+//boostrap files
 import 'bootstrap/dist/js/bootstrap.bundle'
 import 'bootstrap/dist/css/bootstrap.min.css'
+import 'bootstrap-icons/font/bootstrap-icons.css';
 
 //Fonts CSS
 import "@/assets/fonts/main.css"
